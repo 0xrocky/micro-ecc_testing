7aaab985c37f398a3986f93ae8106fdab13dd23e
Michele Corrias <9300586+0xrocky@users.noreply.github.com>
2021-07-09 01:55:00 +0200

    Add files via upload

---
+++ Debug/Core/Src/uECC.d
@@ -0,0 +1,22 @@
+Core/Src/uECC.o: ../Core/Src/uECC.c \
+ C:/Users/Giorgia\ Bettinelli/STM32CubeIDE/workspace_1.6.0/micro-ecc/Core/Inc/micro-ecc/uECC.h \
+ C:/Users/Giorgia\ Bettinelli/STM32CubeIDE/workspace_1.6.0/micro-ecc/Core/Inc/micro-ecc/uECC_vli.h \
+ C:/Users/Giorgia\ Bettinelli/STM32CubeIDE/workspace_1.6.0/micro-ecc/Core/Inc/micro-ecc/uECC.h \
+ C:/Users/Giorgia\ Bettinelli/STM32CubeIDE/workspace_1.6.0/micro-ecc/Core/Inc/micro-ecc/types.h \
+ C:/Users/Giorgia\ Bettinelli/STM32CubeIDE/workspace_1.6.0/micro-ecc/Core/Inc/micro-ecc/platform-specific.inc \
+ C:/Users/Giorgia\ Bettinelli/STM32CubeIDE/workspace_1.6.0/micro-ecc/Core/Inc/micro-ecc/asm_arm.inc \
+ C:/Users/Giorgia\ Bettinelli/STM32CubeIDE/workspace_1.6.0/micro-ecc/Core/Inc/micro-ecc/curve-specific.inc
+
+C:/Users/Giorgia\ Bettinelli/STM32CubeIDE/workspace_1.6.0/micro-ecc/Core/Inc/micro-ecc/uECC.h:
+
+C:/Users/Giorgia\ Bettinelli/STM32CubeIDE/workspace_1.6.0/micro-ecc/Core/Inc/micro-ecc/uECC_vli.h:
+
+C:/Users/Giorgia\ Bettinelli/STM32CubeIDE/workspace_1.6.0/micro-ecc/Core/Inc/micro-ecc/uECC.h:
+
+C:/Users/Giorgia\ Bettinelli/STM32CubeIDE/workspace_1.6.0/micro-ecc/Core/Inc/micro-ecc/types.h:
+
+C:/Users/Giorgia\ Bettinelli/STM32CubeIDE/workspace_1.6.0/micro-ecc/Core/Inc/micro-ecc/platform-specific.inc:
+
+C:/Users/Giorgia\ Bettinelli/STM32CubeIDE/workspace_1.6.0/micro-ecc/Core/Inc/micro-ecc/asm_arm.inc:
+
+C:/Users/Giorgia\ Bettinelli/STM32CubeIDE/workspace_1.6.0/micro-ecc/Core/Inc/micro-ecc/curve-specific.inc:
