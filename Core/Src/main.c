e4585d6522032f013e24d4b3e63a50b5468480e4
Michele Corrias <9300586+0xrocky@users.noreply.github.com>
2021-07-23 03:19:07 +0200

    Add files via upload

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -26,34 +26,33 @@
 /* USER CODE BEGIN Includes */
 #include "micro-ecc/uECC.h"
 /* USER CODE END Includes */
-
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
 /* USER CODE END PTD */
-
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define WELCOME "Welcome to the micro-ecc Discovery menu\r\n"
-#define MENU "Select the option you want:\r\n1: Toggle a led\r\n2: Read the led status\r\n3: Make keys with ECC and sign\r\n4: Clear screen and print this message"
+#define WELCOME "*** Welcome to the micro-ecc STM32F407VG Discovery menu ***\r\n"
+#define MENU "Select the option you want:\r\n1: Toggle a led\r\n2: Read the led status\r\n3: Create a uECC_secp160r1 curve and make a key couple\r\n4: Sign an \"HELLO\" message with micro-ecc\r\n5: Verify the previous sign with micro-ecc\r\n6: Clear your screen and print this message"
 #define PROMPT "\r\n< "
+#define RESET "\033[0;0H"	/* Reset screen point to the top */
+#define CLEAR "\033[2J"		/* Clear out the terminal program */
 /* USER CODE END PD */
-
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
+#define FLAG_MAKE 0
+#define FLAG_SIGN 1
 /* USER CODE END PM */
-
 /* Private variables ---------------------------------------------------------*/
 RNG_HandleTypeDef hrng;
 UART_HandleTypeDef huart2;
-
 /* USER CODE BEGIN PV */
 uECC_Curve curve;
 uint8_t privateKey[21] = { 0 }; /* 21 bytes long with first byte 0 in secp160r1 */
 uint8_t publicKey[40] = { 0 }; /* 2 * curve size bytes long (secp160r1: 160*2/8) */
-uint8_t hashMsg[32] = { 0x2C, 0xF2, 0x4D, 0xBA, 0x5F, 0xB0, 0xA3, 0x0E, 0x26, 0xE8, 0x3B, 0x2A, 0xC5, 0xB9, 0xE2, 0x9E, 0x1B, 0x16, 0x1E, 0x5C, 0x1F, 0xA7, 0x42, 0x5E, 0x73, 0x04, 0x33, 0x62, 0x93, 0x8B, 0x98, 0x24 }; /* "Hello" SHA-256 hash */
+const uint8_t hashMsg[32] = { 0x2C, 0xF2, 0x4D, 0xBA, 0x5F, 0xB0, 0xA3, 0x0E, 0x26, 0xE8, 0x3B, 0x2A, 0xC5, 0xB9, 0xE2, 0x9E, 0x1B, 0x16, 0x1E, 0x5C, 0x1F, 0xA7, 0x42, 0x5E, 0x73, 0x04, 0x33, 0x62, 0x93, 0x8B, 0x98, 0x24 }; /* "Hello" SHA-256 hash */
 uint8_t signHash[40] = { 0 };
+uint8_t flags;
 /* USER CODE END PV */
-
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
@@ -61,15 +60,23 @@ static void MX_USART2_UART_Init(void);
 static void MX_RNG_Init(void);
 /* USER CODE BEGIN PFP */
 void printWelcomeMsg( void );
-uint8_t readeUserInput( void );
-uint8_t processUserInput( uint8_t opt );
+uint8_t readUserInput( void );
+uint8_t processUserInput( uint8_t choice );
 static int trng( uint8_t *dest, unsigned size );
+static void forEachByteArray( uint8_t *array, size_t size );
+static void flag_make_set();
+static void flag_make_clr();
+static uint8_t flag_make_get();
+static void flag_sign_set();
+static void flag_sign_clr();
+static uint8_t flag_sign_get();
+static void flag_verify_set();
+static void flag_verify_clr();
+static uint8_t flag_verify_get();
 /* USER CODE END PFP */
-
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 /* USER CODE END 0 */
-
 /**
   * @brief  The application entry point.
   * @retval int
@@ -77,7 +84,7 @@ static int trng( uint8_t *dest, unsigned size );
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-	uint8_t opt = 0;
+	uint8_t choice = 0, tempcode = 0;
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -99,7 +106,7 @@ int main(void)
   MX_USART2_UART_Init();
   MX_RNG_Init();
   /* USER CODE BEGIN 2 */
-  /* DWT */
+  /* DWT for counting CPU cycles */
   volatile uint32_t *DWT_CONTROL = (uint32_t *) 0xE0001000;
   volatile uint32_t *DWT_CYCCNT = (uint32_t *) 0xE0001004;
   volatile uint32_t *DEMCR = (uint32_t *) 0xE000EDFC;
@@ -107,112 +114,149 @@ int main(void)
   DWT->CYCCNT = 0;
   DWT->CTRL |= DWT_CTRL_CYCCNTENA_Msk;
 
+  /* Creating the uECC curve */
   #if uECC_SUPPORTS_secp160r1
   	  curve = uECC_secp160r1();
   #endif
+  /* Setting the random number generator function used by the curve */
+  uECC_set_rng( &trng );
 
   /* USER CODE END 2 */
-printMessage:
-	printWelcomeMsg();
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  opt = readeUserInput();
-	  int tempCode = processUserInput( opt );
-	  if ( opt == 4 && tempCode == 2 )
-		  goto printMessage;
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
+	printWelcomeMsg();
+	do
+	{
+		choice = readUserInput();
+		tempcode = processUserInput( choice );
+	}
+	while( !( choice == 6 && tempcode == 2 ) );
   }
+  /* USER CODE END WHILE */
+
+  /* USER CODE BEGIN 3 */
   /* USER CODE END 3 */
 }
-
+/* Function that prints on video the welcome message */
 void printWelcomeMsg( void )
 {
-	HAL_UART_Transmit( &huart2, ( uint8_t* )"\033[0;0H", strlen( "\033[0;0H" ), HAL_MAX_DELAY ); /* Reset screen point to the top */
-	HAL_UART_Transmit( &huart2, ( uint8_t* )"\033[2J", strlen( "\033[2J" ), HAL_MAX_DELAY ); /* Clear out the terminal program */
+	HAL_UART_Transmit( &huart2, ( uint8_t* )RESET, strlen( RESET ), HAL_MAX_DELAY );
+	HAL_UART_Transmit( &huart2, ( uint8_t* )CLEAR, strlen( CLEAR ), HAL_MAX_DELAY );
 	HAL_UART_Transmit( &huart2, ( uint8_t* )WELCOME, strlen ( WELCOME ), HAL_MAX_DELAY );
 	HAL_UART_Transmit( &huart2, ( uint8_t* )MENU, strlen ( MENU ), HAL_MAX_DELAY );
 }
-
-uint8_t readeUserInput( void )
+/* Function that reads the input of the user expected as a single number */
+uint8_t readUserInput( void )
 {
-	char buf[ 1 ];
+	char buf;
 	HAL_UART_Transmit( &huart2, ( uint8_t* )PROMPT, strlen ( PROMPT ), HAL_MAX_DELAY );
-	HAL_UART_Receive( &huart2, ( uint8_t* )buf, 1, HAL_MAX_DELAY );
-	return atoi( buf );
+	HAL_UART_Receive( &huart2, ( uint8_t* )&buf, 1, HAL_MAX_DELAY );
+	return atoi( &buf );
 }
-
-uint8_t processUserInput( uint8_t opt )
+/* Function that processes the input of the user executing a specific procedure driven by the number chosen */
+uint8_t processUserInput( uint8_t choice )
 {
 	char msg[ 40 ];
-	char buf[ 1 ];
 	uint32_t start = 0, end = 0, elapsed = 0;
+	uint8_t result = 0;
 
-	if ( opt < 1 || opt > 4 )
+	if ( choice < 1 || choice > 6 )
 		return 0;
 
-	sprintf( msg, "%d", opt );
+	sprintf( msg, "%d", choice );
 	HAL_UART_Transmit( &huart2, ( uint8_t* )msg, strlen ( msg ), HAL_MAX_DELAY );
 
-	switch ( opt )
+	switch ( choice )
 	{
+		/* Toggle a led */
 		case 1:
 			start = DWT->CYCCNT;
 			HAL_GPIO_TogglePin( GPIOD, GPIO_PIN_15 );
 			end = DWT->CYCCNT;
 			break;
+		/* Read a led status */
 		case 2:
 			start = DWT->CYCCNT;
-			sprintf( msg, "\r\nLED status: %s", HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_15) == GPIO_PIN_SET ? "ON" : "OFF" );
+			result = HAL_GPIO_ReadPin(GPIOD, GPIO_PIN_15); /* uint8_t vs uint16_t */
 			end = DWT->CYCCNT;
+			sprintf( msg, "\r\nLED status: %s", result == GPIO_PIN_SET ? "ON" : "OFF" );
 			HAL_UART_Transmit( &huart2, ( uint8_t* )msg, strlen ( msg ), HAL_MAX_DELAY );
 			break;
+		/* Create a curve and make a key pair */
 		case 3:
 			start = DWT->CYCCNT;
-			uECC_set_rng( &trng );
-			if ( uECC_make_key( publicKey, privateKey, curve ) )
-				sprintf( msg, "\r\nuECC make key success!\r\n" );
-			else
-				sprintf( msg, "\r\nuECC make key failure!\r\n" );
-			HAL_UART_Transmit( &huart2, ( uint8_t* )msg, strlen ( msg ), HAL_MAX_DELAY );
-
-			HAL_UART_Transmit( &huart2, ( uint8_t* )"\r\nThe HELLO SHA-256 hash: ", strlen ( "\r\nThe HELLO SHA-256 hash: " ), HAL_MAX_DELAY );
-			for ( int i = 0; i < sizeof( hashMsg ); i++ )
-			{
-				HAL_UART_Transmit( &huart2, ( uint8_t* )"0x", strlen( "0x" ), HAL_MAX_DELAY );
-				sprintf( buf, "%.2X", hashMsg[ i ] );
-				HAL_UART_Transmit( &huart2, ( uint8_t* )buf, 1, HAL_MAX_DELAY );
-			}
-			HAL_UART_Transmit( &huart2, ( uint8_t* )"\r\n", strlen ( "\r\n" ), HAL_MAX_DELAY );
-
-
-			if ( uECC_sign( privateKey, hashMsg, sizeof( hashMsg ), signHash, curve ) )
-				sprintf( msg, "\r\nuECC sign success!\r\n" );
-			else
-				sprintf( msg, "\r\nuECC sign failure!\r\n" );
-			HAL_UART_Transmit( &huart2, ( uint8_t* )msg, strlen ( msg ), HAL_MAX_DELAY );
-
-			HAL_UART_Transmit( &huart2, ( uint8_t* )"\r\nThe sign of the HELLO SHA-256 hash: ", strlen ( "\r\nThe sign of the HELLO SHA-256 hash: " ), HAL_MAX_DELAY );
-			for ( int i = 0; i < sizeof( signHash ); i++ )
-			{
-				HAL_UART_Transmit( &huart2, ( uint8_t* )"0x", strlen( "0x" ), HAL_MAX_DELAY );
-				sprintf( buf, "%.X", signHash[ i ] );
-				HAL_UART_Transmit( &huart2, ( uint8_t* )buf, 1, HAL_MAX_DELAY );
-			}
-			HAL_UART_Transmit( &huart2, ( uint8_t* )"\r\n", strlen ( "\r\n" ), HAL_MAX_DELAY );
-
-			if ( uECC_verify( publicKey, hashMsg, sizeof( hashMsg ), signHash, curve ) )
-				sprintf( msg, "\r\nuECC verify success!\r\n" );
-			else
-				sprintf( msg, "\r\nuECC verify failure!\r\n" );
-			HAL_UART_Transmit( &huart2, ( uint8_t* )msg, strlen ( msg ), HAL_MAX_DELAY );
-
+			result = uECC_make_key( publicKey, privateKey, curve );
 			end = DWT->CYCCNT;
+			if ( !result )
+			{
+		        flag_make_clr(); /* clear the make flags */
+				sprintf( msg, "\r\nuECC make key failure!" );
+			}
+			else
+			{
+				flag_make_set(); /* set the make flags */
+				sprintf( msg, "\r\nuECC make key success!" );
+			}
+			HAL_UART_Transmit( &huart2, ( uint8_t* )msg, strlen ( msg ), HAL_MAX_DELAY );
+
+			sprintf( msg, "\r\nThe public key is: " );
+			HAL_UART_Transmit( &huart2, ( uint8_t* )msg, strlen ( msg ), HAL_MAX_DELAY );
+			forEachByteArray( publicKey, sizeof ( publicKey ) );
+			sprintf( msg, "\r\nThe private key is: " );
+			HAL_UART_Transmit( &huart2, ( uint8_t* )msg, strlen ( msg ), HAL_MAX_DELAY );
+			forEachByteArray( privateKey, sizeof ( privateKey ) );
 			break;
+		/* Signing */
 		case 4:
+			if( !( flag_make_get() ) ) /* verify the make flags */
+			{
+				sprintf( msg, "\r\nKeys pair not created!" );
+				HAL_UART_Transmit( &huart2, ( uint8_t* )msg, strlen ( msg ), HAL_MAX_DELAY );
+				return 2;
+			}
+			sprintf( msg, "\r\n\"HELLO\" SHA-256 hash: " );
+			HAL_UART_Transmit( &huart2, ( uint8_t* )msg, strlen ( msg ), HAL_MAX_DELAY );
+			forEachByteArray( hashMsg, sizeof ( hashMsg ) );
+
+			start = DWT->CYCCNT;
+			result = uECC_sign( privateKey, hashMsg, sizeof( hashMsg ), signHash, curve );
+			end = DWT->CYCCNT;
+			if ( !result )
+			{
+		        flag_sign_clr(); /* clear the sign flags */
+				sprintf( msg, "\r\nuECC sign failure!" );
+			}
+			else
+			{
+		        flag_sign_set(); /* set the sign flags */
+				sprintf( msg, "\r\nuECC sign success!" );
+			}
+			HAL_UART_Transmit( &huart2, ( uint8_t* )msg, strlen ( msg ), HAL_MAX_DELAY );
+
+			sprintf( msg, "\r\nThe sign of the HELLO SHA-256 hash: " );
+			HAL_UART_Transmit( &huart2, ( uint8_t* )msg, strlen ( msg ), HAL_MAX_DELAY );
+			forEachByteArray( signHash, sizeof( signHash ) );
+			break;
+			/* Verifying */
+			case 5:
+			if( !( flag_sign_get() ) ) /* verify the sign flags */
+			{
+				sprintf( msg, "\r\nMessage not signed!" );
+				HAL_UART_Transmit( &huart2, ( uint8_t* )msg, strlen ( msg ), HAL_MAX_DELAY );
+				return 2;
+			}
+			start = DWT->CYCCNT;
+			result = uECC_verify( publicKey, hashMsg, sizeof( hashMsg ), signHash, curve );
+			end = DWT->CYCCNT;
+			if ( !result )
+				sprintf( msg, "\r\nuECC verify of sign failure!" );
+			else
+				sprintf( msg, "\r\nuECC verify of sign success!" );
+			HAL_UART_Transmit( &huart2, ( uint8_t* )msg, strlen ( msg ), HAL_MAX_DELAY );
+			break;
+		case 6:
 			HAL_Delay( 1000 );
 			return 2;
 	}
@@ -220,19 +264,19 @@ uint8_t processUserInput( uint8_t opt )
 	if ( end >= start )
 	{
 		elapsed = end - start;
-		sprintf( msg, "\r\n%lu cycles", elapsed );
+		sprintf( msg, "\r\n%lu CPU clock cycles", elapsed );
 		HAL_UART_Transmit( &huart2, ( uint8_t* )msg, strlen ( msg ), HAL_MAX_DELAY );
 		return 1;
 	}
 	else
 		return 0;
 }
-
+/* Function that sets a true random number generator for the micro-ecc lib */
 static int trng( uint8_t *dest, unsigned size )
 {
 	while ( size )
 	{
-		/* uint8_t random = rand() % 1000 + 1;*/
+		/* uint8_t random = rand() % 1000 + 1; */
 		uint32_t random = 0;
 		if ( HAL_RNG_GenerateRandomNumber( &hrng, &random ) != HAL_OK )
 			Error_Handler();
@@ -242,7 +286,67 @@ static int trng( uint8_t *dest, unsigned size )
 	}
 	return 1;
 }
-
+/* Support function that prints on video from UART an array byte by byte */
+static void forEachByteArray( uint8_t *array, size_t size )
+{
+	char msg[ 40 ];
+	for ( int i = 0; i < size; i++ )
+	{
+		HAL_UART_Transmit( &huart2, ( uint8_t* )"0x", strlen( "0x" ), HAL_MAX_DELAY );
+		sprintf( msg, "%.2X", array[ i ] );
+		HAL_UART_Transmit( &huart2, ( uint8_t* )msg, strlen( msg ), HAL_MAX_DELAY );
+		if ( i != size - 1 )
+			HAL_UART_Transmit( &huart2, ( uint8_t* )", ", strlen( ", " ), HAL_MAX_DELAY );
+	}
+	HAL_UART_Transmit( &huart2, ( uint8_t* )"\r\n", strlen ( "\r\n" ), HAL_MAX_DELAY );
+}
+/* Support function that set a flag if the keys pair is made */
+static void flag_make_set()
+{
+	flags |= ( 1 << FLAG_MAKE );
+}
+/* Support function that reset a flag for the keys pair */
+static void flag_make_clr()
+{
+	flags &= ~( 1 << FLAG_MAKE );
+}
+/* Support function that get a flag if the keys pair is created */
+static uint8_t flag_make_get()
+{
+	return flags & ( 1 << FLAG_MAKE );
+}
+/* Support function that set a flag if the sign is made */
+static void flag_sign_set()
+{
+	flags |= ( 1 << FLAG_SIGN );
+}
+/* Support function that reset a flag for the sign */
+static void flag_sign_clr()
+{
+	flags &= ~( 1 << FLAG_SIGN );
+}
+/* Support function that get a flag if the sign is made */
+static uint8_t flag_sign_get()
+{
+	return flags & ( 1 << FLAG_SIGN );
+}
+/* Support function that set a flag if the verify sign is made */
+static void flag_verify_set()
+{
+	flags |= ( 1 << FLAG_MAKE );
+	flags |= ( 1 << FLAG_SIGN );
+}
+/* Support function that reset a flag for the verify sign */
+static void flag_verify_clr()
+{
+	flags &= ~( 1 << FLAG_MAKE );
+	flags &= ~( 1 << FLAG_SIGN );
+}
+/* Support function that get a flag if the verify sign is made */
+static uint8_t flag_verify_get()
+{
+	return flags & ( 1 << FLAG_SIGN ) & ( 1 << FLAG_MAKE );
+}
 /**
   * @brief System Clock Configuration
   * @retval None
@@ -251,7 +355,6 @@ void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
   /** Configure the main internal regulator output voltage
   */
   __HAL_RCC_PWR_CLK_ENABLE();
@@ -286,7 +389,6 @@ void SystemClock_Config(void)
     Error_Handler();
   }
 }
-
 /**
   * @brief RNG Initialization Function
   * @param None
@@ -307,7 +409,6 @@ static void MX_RNG_Init(void)
   /* USER CODE BEGIN RNG_Init 2 */
   /* USER CODE END RNG_Init 2 */
 }
-
 /**
   * @brief USART2 Initialization Function
   * @param None
@@ -335,7 +436,6 @@ static void MX_USART2_UART_Init(void)
   /* USER CODE BEGIN USART2_Init 2 */
   /* USER CODE END USART2_Init 2 */
 }
-
 /**
   * @brief GPIO Initialization Function
   * @param None
@@ -359,10 +459,8 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 }
-
 /* USER CODE BEGIN 4 */
 /* USER CODE END 4 */
-
 /**
   * @brief  This function is executed in case of error occurrence.
   * @retval None
@@ -377,7 +475,6 @@ void Error_Handler(void)
   }
   /* USER CODE END Error_Handler_Debug */
 }
-
 #ifdef  USE_FULL_ASSERT
 /**
   * @brief  Reports the name of the source file and the source line number
@@ -394,5 +491,4 @@ void assert_failed(uint8_t *file, uint32_t line)
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
-
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
